2d8710108ca253dd660d016a97d08228054f58be
kraihn <jared@dunesky.com>
2011-08-24 14:46:54 -0400

    Reads the number of lines as specified in the arguments into an array.

--- bilateral-projects.py
+++ bilateral-projects.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python
+import sys
 
 def main():
-	print 'main'
+	lines = int(sys.argv[1])
+	
+	input = []
+	
+	for i in range(0,lines):
+		input.append(sys.stdin.readline().replace('\n', ''))
+		
+	print input
 
 if __name__ == '__main__':
 	main()
