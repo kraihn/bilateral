c0e3a6add1af7c7b58c0426fd73eeb1262a571fd
kraihn <jared@dunesky.com>
2011-08-24 21:37:39 -0400

    Project class created and used for input items.

--- bilateral-projects.py
+++ bilateral-projects.py
@@ -1,14 +1,27 @@
 #! /usr/bin/env python
 import sys
 
+
+class Project():
+	pass
+	
+
 def main():
 	lines = int(sys.stdin.readline().replace('\n', ''))
 	
-	input = []	
+	items = []	
 	for i in range(0,lines):
-		input.append(sys.stdin.readline().replace('\n', ''))
+		line = sys.stdin.readline().replace('\n', '').split(' ')
+				
+		item = Project()
+		item.id = i
+		item.stockholm = line[0]
+		item.london = line[1]
 		
-	print input
+		items.append(item)
+		#print str(item.id) + ' ' + item.stockholm + ' ' + item.london
+		
+	#print items
 
 if __name__ == '__main__':
 	main()
